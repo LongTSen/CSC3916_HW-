210fe16067f60995e01ac6fe304e22beae5de248
LongSen <senthanglong92@gmail.com>
2020-03-15 01:14:59 -0600

    commit

---
+++ auth.js
@@ -0,0 +1,30 @@
+var passport       =  require( 'passport' );
+var BasicStrategy  =  require( 'passport-http' ).BasicStrategy;
+var userController =  require( './usercontroller' );
+var crypto         =  require( 'crypto' );
+
+// Authentication
+passport.use(
+    new BasicStrategy(
+        function( username , password , done )
+        {
+            // Retrieve
+            userController.findUserByLogin( username , password )
+                .then(
+                    function( user )
+                    {
+                        // Found
+                        if ( user )
+                        {
+                            return done( null , { name: user.username } );
+                        }
+                        // Fail otherwise
+                        else
+                        {
+                            return done( null , false );
+                        }
+                    });
+        }
+    ));
+
+exports.isAuthenticated  =  passport.authenticate( 'basic' , { session : false } );
---
+++ movie.js
@@ -0,0 +1,32 @@
+var mongoose  =  require( 'mongoose' );
+var Schema    =  mongoose.Schema;
+
+var movieSchema  =  new Schema({
+    title        :	{
+        type     : String,
+        required : true,
+        unique   : true
+    },
+    yearReleased :	{
+        type     : String,
+        required : true
+    },
+    genre        :	{
+        type     : String,
+        enum     : [ "Action", "Adventure", "Comedy", "Drama", "Fantasy", "Horror", "Mystery", "Thriller", "Western" ],
+        required : true
+    },
+    actors       :	{
+        type     :	Array,
+        required :	true,
+        items    :	{
+            actorName     : String,
+            characterName : String
+        },
+        minItems : 3
+    }
+});
+
+var Movie  =  mongoose.model( 'Movie' , movieSchema );
+
+module.exports  =  Movie;
